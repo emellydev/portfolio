5770506cfecf67dc2334e745ad3651a0df9fb639
Emelly Beatriz <emellybmuniz@gmail.com>
2025-08-14 19:34:57 -0300

    Add certificates carousel and refactor skills section
    Introduced a new certificates section with an animated carousel to showcase certificate images and links. Refactored the skills section to use a new image path and improved its styling. Updated CSS for both sections, enhanced responsive design, and improved JavaScript for UI effects and certificate carousel animation.

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,58 @@
-const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
-const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+const botaoMostrarProjetos = document.querySelector(".btn-mostrar-projetos");
+const projetosInativos = document.querySelectorAll(".projeto:not(.ativo)");
 
-botaoMostrarProjetos.addEventListener('click', () => {
-    const botaoEstaAtivo = botaoMostrarProjetos.classList.contains('ativo');
+botaoMostrarProjetos.addEventListener("click", () => {
+  const botaoEstaAtivo = botaoMostrarProjetos.classList.contains("ativo");
 
-    if (botaoEstaAtivo) {
-        esconderProjetos();
-    } else {
-        mostrarMaisProjetos();
-    }
+  if (botaoEstaAtivo) {
+    esconderProjetos();
+  } else {
+    mostrarMaisProjetos();
+  }
 });
 
+// Esconder os projetos
 function esconderProjetos() {
-    projetosInativos.forEach(projetoInativo => {
-        projetoInativo.classList.remove('ativo');
-    });
-    botaoMostrarProjetos.classList.remove('ativo');
-    botaoMostrarProjetos.textContent = 'Mostrar mais';
+  projetosInativos.forEach((projetoInativo) => {
+    projetoInativo.classList.remove("ativo");
+  });
+  botaoMostrarProjetos.classList.remove("ativo");
+  botaoMostrarProjetos.textContent = "Mostrar mais";
 }
 
+// Mostrar mais projetos
 function mostrarMaisProjetos() {
-    projetosInativos.forEach(projetoInativo => {
-        projetoInativo.classList.add('ativo');
-    });
-    botaoMostrarProjetos.classList.add('ativo');
-    botaoMostrarProjetos.textContent = 'Mostrar menos';
+  projetosInativos.forEach((projetoInativo) => {
+    projetoInativo.classList.add("ativo");
+  });
+  botaoMostrarProjetos.classList.add("ativo");
+  botaoMostrarProjetos.textContent = "Mostrar menos";
 }
 
-
-document.documentElement.addEventListener('mousemove', (e) => {
-    document.documentElement.style.setProperty('--mouse-x', `${e.clientX}px`);
-    document.documentElement.style.setProperty('--mouse-y', `${e.clientY}px`);
+// Efeito de parallax
+document.documentElement.addEventListener("mousemove", (e) => {
+  document.documentElement.style.setProperty("--mouse-x", `${e.clientX}px`);
+  document.documentElement.style.setProperty("--mouse-y", `${e.clientY}px`);
 });
 
-
-const text = 'Olá! sou Emelly Beatriz |'
-const typedE1 = document.querySelector('.typed');
+// Efeito de digitação
+const text = "Olá! sou a Emelly Beatriz.";
+const typedE1 = document.querySelector(".typed");
 
 let i = 0;
 
 function type() {
-    if (i < text.length) {
-        typedE1.textContent += text.charAt(i);
-        i++;
-        setTimeout(type, 120);
-    }
+  if (i < text.length) {
+    typedE1.textContent += text.charAt(i);
+    i++;
+    setTimeout(type, 120);
+  }
 }
-window.addEventListener('DOMContentLoaded', type);
+window.addEventListener("DOMContentLoaded", type);
 
-
+// Efeito slide certificados
+window.addEventListener("load", () => {
+    const slide = document.querySelector(".certificates-slide");
+    const slideContent = slide.innerHTML;
+    slide.innerHTML += slideContent; 
+});
